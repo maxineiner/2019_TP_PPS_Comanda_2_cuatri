3aac3f4b0114a84a225b3fe5d32b968677135750
Lautaro01 <lau123lau321@gmail.com>
2019-10-31 14:59:19 -0300

    Agrego login v1.0.0 (splash vacio)

--- proyectoLaComandada/src/app/app-routing.module.ts
+++ proyectoLaComandada/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
+
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {path:'', component:LoginComponent},
 ];
 
 @NgModule({
--- proyectoLaComandada/src/app/app.module.ts
+++ proyectoLaComandada/src/app/app.module.ts
@@ -8,9 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './componentes/login/login.component';
+
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,
+  LoginComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
