3f79b3f16136768cad8c989bb3d073d4600d2788
salibacristian <chriseze21@gmail.com>
2019-11-02 00:41:56 -0300

    logout

--- Comanda/src/app/app.component.ts
+++ Comanda/src/app/app.component.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { timer } from 'rxjs';
+import { AuthService } from "./services/auth.service";
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -16,7 +19,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private AFauth: AuthService,
+    private publicRouter:Router
   ) {
     this.initializeApp();
   }
@@ -27,5 +32,14 @@ export class AppComponent {
       this.splashScreen.hide();
       timer(3600).subscribe(()=>{this.showSplash=false;})
     });
+  }
+
+  LogOut(){
+    this.AFauth.logOut();
+    this.publicRouter.navigate(['/log-in'])
+  }
+
+  home(){
+    this.publicRouter.navigate(['/home'])
   }
 }
