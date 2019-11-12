2597bcf40b59421a85bdda329897a979fa684c76
Lautaro01 <lau123lau321@gmail.com>
2019-11-12 19:23:27 -0300

    Puliendo login, agrego select

---
+++ proyectoLaComandada/src/app/componentes/menu-cliente/menu-cliente.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-cliente',
+  templateUrl: './menu-cliente.component.html',
+  styleUrls: ['./menu-cliente.component.scss'],
+})
+export class MenuClienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
