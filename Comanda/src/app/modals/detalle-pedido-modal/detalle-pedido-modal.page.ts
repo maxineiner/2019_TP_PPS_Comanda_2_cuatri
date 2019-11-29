33baa92247967bc53623de72440911c3048a8996
salibacristian <chriseze21@gmail.com>
2019-11-29 00:00:19 -0300

    confirmar y cancelar pedido

--- Comanda/src/app/modals/detalle-pedido-modal/detalle-pedido-modal.page.ts
+++ Comanda/src/app/modals/detalle-pedido-modal/detalle-pedido-modal.page.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Pedido } from '../../model/pedido';
-import { NavParams, AlertController, ModalController } from '@ionic/angular';
+import { ModalController, AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { PedidosService } from '../../services/pedidos.service';
-
 
 
 @Component({
@@ -18,25 +17,37 @@ export class DetallePedidoModalPage implements OnInit {
 
   constructor(
     public router: Router,
-    public modalCtrl: ModalController,
+    public alertController: AlertController,
+    public modalController: ModalController,
     private pedidosService: PedidosService) { }
 
-  ngOnInit() {  
+  ngOnInit() {
   }
 
-  cambiarEstado(pedido: Pedido, estado: string){
-    this.pedidosService.SetEstado(pedido.id, estado);
+  async cambiarEstado(pedido: Pedido, estado: string) {
+    await this.pedidosService.SetEstado(pedido.id, estado);
+    this.presentAlert(estado);
   }
-  
+
 
   dismiss() {
     // using the injected ModalController this page
     // can "dismiss" itself and optionally pass back data
-    this.modalCtrl.dismiss({
+    this.modalController.dismiss({
       'dismissed': true
     });
   }
 
+  async presentAlert(estado: string) {
+    const alert = await this.alertController.create({
+      header: estado,
+      message: 'operación realizada exitosamente'    
+    });
+
+    await alert.present();
+
+    this.dismiss();
+  }
 }
 
 
