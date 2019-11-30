775e5103bec8c6c29a8d46c19d2628f244296bf0
Hernán Perez <HPEREZ@gire.com>
2019-11-30 05:52:52 -0300

    se verifica el estado del pedido y si es entregado se redirecciona auna nueva page

--- Comanda/src/app/services/pedidos.service.ts
+++ Comanda/src/app/services/pedidos.service.ts
@@ -10,15 +10,15 @@ export class PedidosService {
 
   constructor(private firestore: AngularFirestore) { }
 
-  public AddPedidoToBD(auxPedido:Pedido) {
+  public AddPedidoToBD(auxPedido: Pedido) {
     console.log(auxPedido);
     this.firestore.collection('Lista_Pedidos').add({
-      idAuth:auxPedido.idAuth,
+      idAuth: auxPedido.idAuth,
       total: auxPedido.total,
       estado: auxPedido.estado,
       arrayDetalle: JSON.stringify(auxPedido.arrayDetalle),
-      propina:0,
-      totalPropina:0
+      propina: 0,
+      totalPropina: 0
 
     });
   }
@@ -27,40 +27,46 @@ export class PedidosService {
     return this.firestore.collection('Lista_Pedidos').snapshotChanges();
   }
 
+  public async getPedido(idAuth) {
+    return await this.firestore.collection('Lista_Pedidos').ref
+      .where('idAuth', '==', idAuth)
+      .get();
+  }
+
   public async SetEstado(id: string, estado: string) {
     await this.firestore.doc('Lista_Pedidos/' + id).set({
       estado: estado
     }, { merge: true });
   }
 
-public getPedidosBase() {
-  return this.firestore.collection('Lista_Pedidos').snapshotChanges().pipe(map((clientes) => {
-    return clientes.map((a) => {
-      const data = a.payload.doc.data() as Pedido;
-      // data.id = a.payload.doc.id;
-      return data;
+  public getPedidosBase() {
+    return this.firestore.collection('Lista_Pedidos').snapshotChanges().pipe(map((clientes) => {
+      return clientes.map((a) => {
+        const data = a.payload.doc.data() as Pedido;
+        // data.id = a.payload.doc.id;
+        return data;
+      });
+    }));
+  }
+  public async SetPropina(auxCliente: Pedido) {
+    console.log(auxCliente.idAuth);
+
+    await this.firestore.collection('Lista_Pedidos').ref.where('idAuth', '==', auxCliente.idAuth).get().then(async (documento) => {
+
+      console.log(documento.docs[0].id);
+      // console.log('EncontrÃ© el voto', votos.users);
+
+      this.firestore.collection('Lista_Pedidos').doc(documento.docs[0].id).set({
+        arrayDetalle: auxCliente.arrayDetalle,
+        estado: auxCliente.estado,
+        idAuth: auxCliente.idAuth,
+        total: auxCliente.total,
+        propina: auxCliente.propina,
+        totalPropina: auxCliente.totalPropina
+      }//, { merge: true }
+      );
+
     });
-  }));
-}
-public async SetPropina(auxCliente:Pedido) {
-   console.log(auxCliente.idAuth);
 
-  await this.firestore.collection('Lista_Pedidos').ref.where('idAuth', '==', auxCliente.idAuth).get().then(async (documento) => {
- 
-    console.log(documento.docs[0].id);
-    // console.log('EncontrÃ© el voto', votos.users);
-    
-    this.firestore.collection('Lista_Pedidos').doc(documento.docs[0].id).set({
-      arrayDetalle:auxCliente.arrayDetalle,
-      estado:auxCliente.estado,
-      idAuth:auxCliente.idAuth,
-      total:auxCliente.total,
-      propina:auxCliente.propina,
-      totalPropina: auxCliente.totalPropina
-    }//, { merge: true }
-    );
-    
-  });
-
-}
+  }
 }
